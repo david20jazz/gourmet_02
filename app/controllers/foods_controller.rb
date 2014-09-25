31498757772c6fd212732f896a904e42cc57b4d3
ubuntu-jazz <david20jazz@gmail.com>
2014-09-25 17:57:13 +0900

    first commit

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,37 @@
+class FoodsController < ApplicationController
+  def posts
+		@posts = Post.all
+	end
+
+  def posts_category
+	  case params[:category]
+    when "korean"
+      @category = "한식"
+    when "japanese"
+      @category = "일식"
+    when "chinese"
+      @category = "중식"
+    else
+      @category = "양식"
+    end
+    @posts = Post.where(category: @category)
+  end
+
+  def show
+  end
+
+  def write
+  end
+
+  def write_complete
+  end
+
+  def edit
+  end
+
+  def edit_complete
+  end
+
+  def delete_complete
+  end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gourmet02::Application.config.session_store :cookie_store, key: '_gourmet02_session'
